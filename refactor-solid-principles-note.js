025f7b9a4d2cbad4774aebe26677c9eb974912b8
dav nguyxn <hoangson091104@gmail.com>
2026-03-01 08:55:05 -0500

    chore: add SOLID refactor note helper

---
+++ refactor-solid-principles-note.js
@@ -0,0 +1,7 @@
+const repoName = 'Customizable-AI-Chatbot';
+
+function describeSolidRefactorNote() {
+  return `${repoName}: prefer small modules with one clear responsibility.`;
+}
+
+module.exports = { describeSolidRefactorNote };
